e6359308f3190970ac0e6ed3f751060cec08f81e
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-11 20:04:04 +0200

    task-01 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const bodyRef = document.querySelector('body');
+const startBtnHandle = document.querySelector('[data-start]');
+const stopBtnHandle = document.querySelector('[data-stop]');
+let flag = false;
+let intervalId;
 
+startBtnHandle.addEventListener('click', () => {
+  if (!flag) {
+    flag = true;
+    intervalId = setInterval(() => {
+      bodyRef.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+});
+
+stopBtnHandle.addEventListener('click', () => {
+  clearInterval(intervalId);
+  flag = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
