0b389496542ebaa3edffddb89a572b1766c1cd36
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-01-12 14:48:05 +0200

    updated

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,26 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const formSubmitHandler = document.querySelector('form');
+
+formSubmitHandler.addEventListener('submit', () => {
+  const firstDelay = document.querySelector('[name="delay"]').value;
+  const delayStep = document.querySelector('[name="step"]').value;
+  const amount = document.querySelector('[name="amount"]').value;
+  let counter = 1;
+
+  while (counter <= amount) {
+    let delay = firstDelay + delayStep * counter;
+    const promiseId = createPromise(promiseId, delay);
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
